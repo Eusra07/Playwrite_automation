935263de9edda45f7dc902208d6ba9449b1bafcd
Eusra <urbiibnat07@gmail.com>
2026-09-10 22:57:33 +0600

    executed until search button

--- pageObjects/POManager.js
+++ pageObjects/POManager.js
@@ -1,14 +1,21 @@
 const { LoginPage } = require("./LoginPage");
+const { EmployeePage } = require("./EmployeePage");
+
 
 class POManager{
 
     constructor(page){
         this.page = page;
         this.loginPage = new LoginPage(this.page);
+        this.employeePage = new EmployeePage(this.page);
     }
 
     getLoginPage(){
         return this.loginPage;
+    }
+
+    getEmployeePage(){
+        return this.employeePage; 
     }
 }
 
