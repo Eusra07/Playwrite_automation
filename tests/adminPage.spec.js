a5840a98879048b097d0e37da09a8486b055a79e
Eusra <urbiibnat07@gmail.com>
2026-09-14 12:58:45 +0600

    Checked if status updated successfully

--- tests/adminPage.spec.js
+++ tests/adminPage.spec.js
@@ -11,18 +11,20 @@ test(`Verify user search working and update user status ${dataset.USERNAME}`, as
     await loginPage.login(dataset.USERNAME,dataset.PASSWORD);
     await loginPage.validLogin();
     
-    
     const employeePage = poManager.getEmployeePage();
     await employeePage.goToEmployeePage();
     const empId = Math.floor(Math.random()*(999999-100)+100).toString();
     const employeeName = `${empData.LAST_NAME}${empId}`;
     await employeePage.addEmployee(empData.FIRST_NAME,`${employeeName}`,empId);
-
+    
+    
     const adminPage = poManager.getAdminPage();
     await adminPage.goToAdminPage();
     await adminPage.addUser(empData.FIRST_NAME,employeeName,empId);
-    await adminPage.searchUser();
+    await adminPage.searchUser('Enabled');
     await adminPage.edit();
-
+    await adminPage.goToAdminPage();
+    await adminPage.searchUser(adminPage.newState);
+    await adminPage.checkEditedInfo();
 
 })
